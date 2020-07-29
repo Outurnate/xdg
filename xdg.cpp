5b500eb9692def90676b36bd98aeb028cbfc07f4
Outurnate <joseph@outurnate.com>
2020-07-28 21:59:09 -0400

    Converted from boost fs api to std fs api

--- xdg.cpp
+++ xdg.cpp
@@ -15,12 +15,10 @@
 
 #include <xdg.h>
 
-#include <boost/algorithm/string.hpp>
-
 #include <cstdlib>
 #include <stdexcept>
 
-namespace fs = boost::filesystem;
+namespace fs = std::filesystem;
 
 namespace
 {
@@ -31,6 +29,24 @@ fs::path throw_if_not_absolute(const fs::path& p)
         return p;
 
     throw std::runtime_error{"Directores MUST be absolute."};
+}
+
+std::vector<std::string> split(const std::string& str, const std::string& delim)
+{
+    std::vector<std::string> tokens;
+    size_t prev = 0, pos = 0;
+    do
+    {
+        pos = str.find(delim, prev);
+        if (pos == std::string::npos)
+            pos = str.length();
+        std::string token = str.substr(prev, pos-prev);
+        if (!token.empty())
+            tokens.push_back(token);
+        prev = pos + delim.length();
+    }
+    while (pos < str.length() && prev < str.length());
+    return tokens;
 }
 
 namespace env
@@ -119,8 +135,7 @@ std::vector<fs::path> xdg::Data::dirs() const
     if (v.empty())
         return {fs::path{"/usr/local/share"}, fs::path{"/usr/share"}};
 
-    std::vector<std::string> tokens;
-    tokens = boost::split(tokens, v, boost::is_any_of(":"));
+    std::vector<std::string> tokens = split(v, ":");
     std::vector<fs::path> result;
     for (const auto& token : tokens)
     {
@@ -144,8 +159,7 @@ std::vector<fs::path> xdg::Config::dirs() const
     if (v.empty())
         return {fs::path{"/etc/xdg"}};
 
-    std::vector<std::string> tokens;
-    tokens = boost::split(tokens, v, boost::is_any_of(":"));
+    std::vector<std::string> tokens = split(v, ":");
     std::vector<fs::path> result;
     for (const auto& token : tokens)
     {
